276d349469bca0c8da55565d26ef8aa72f055d9e
coolproject2020 <62443893+coolproject2020@users.noreply.github.com>
2020-03-25 22:14:39 +0000

    aa

--- data/creaturescripts/scripts/mastervoice/mageDeath.lua
+++
@@ -1,5 +0,0 @@
-dofile('data/lib/quests/their_masters_voice.lua')
-
-function onDeath(creature, corpse, killer, mostDamageKiller, lastHitUnjustified)
-    return Mage_onDeath(creature, corpse, killer, mostDamageKiller, lastHitUnjustified)
-end
--- data/creaturescripts/scripts/mastervoice/servantDeath.lua
+++
@@ -1,5 +0,0 @@
-dofile('data/lib/quests/their_masters_voice.lua')
-
-function onDeath(creature, corpse, killer, mostDamageKiller, lastHitUnjustified)
-    return Servant_onDeath(creature, corpse, killer, mostDamageKiller, lastHitUnjustified)
-end
